0f410db72eca5efe7f157a175c7a75545d7677e7
Yucked <mail@dvlp.nill.sh>
2024-05-21 21:45:39 -0400

    removed queue from LavaPlayer & publish

--- src/LavaPlayer.cs
+++ src/LavaPlayer.cs
@@ -17,48 +17,41 @@ public class LavaPlayer<TLavaTrack>
     /// </summary>
     [JsonPropertyName("guildId"), JsonInclude, JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
     public ulong GuildId { get; internal init; }
-
+    
     /// <summary>
     /// 
     /// </summary>
     /// TODO: API doesn't return it this way
     [JsonPropertyName("track"), JsonInclude, JsonConverter(typeof(LavaTrackConverter))]
     public LavaTrack Track { get; internal init; }
-
+    
     /// <summary>
     /// 
     /// </summary>
     [JsonPropertyName("volume"), JsonInclude]
     public int Volume { get; internal init; }
-
+    
     /// <summary>
     /// 
     /// </summary>
     [JsonPropertyName("paused"), JsonInclude]
     public bool IsPaused { get; internal init; }
-
+    
     /// <summary>
     /// 
     /// </summary>
     [JsonPropertyName("filters"), JsonInclude]
     public Filters Filters { get; }
-
+    
     /// <summary>
     /// 
     /// </summary>
     [JsonPropertyName("voice"), JsonInclude]
     public VoiceState VoiceState { get; internal init; }
-
+    
     /// <summary>
     /// 
     /// </summary>
     [JsonPropertyName("state"), JsonInclude]
     public PlayerState State { get; internal init; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// TODO: How to handle queue?
-    [Obsolete("Use LavaPlayerExtensions.Queue or LavaPlayer#GetQueue on LavaPlayer instance")]
-    public LavaQueue<TLavaTrack> Queue { get; }
 }
