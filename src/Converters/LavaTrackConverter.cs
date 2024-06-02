7f62d585301f6883326926058a407eb06262fb04
Yucked <mail@dvlp.nill.sh>
2024-06-02 13:01:44 -0400

    code formatting in converters

--- src/Converters/LavaTrackConverter.cs
+++ src/Converters/LavaTrackConverter.cs
@@ -6,20 +6,19 @@ namespace Victoria.Converters;
 
 internal sealed class LavaTrackConverter : JsonConverter<LavaTrack> {
     public override LavaTrack Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
-        using (JsonDocument trackDocument = JsonDocument.ParseValue(ref reader))
-        {
-            trackDocument.RootElement.TryGetProperty("encoded", out JsonElement trackHashElement);
-            trackDocument.RootElement.TryGetProperty("info", out JsonElement trackElement);
-            trackDocument.RootElement.TryGetProperty("pluginInfo", out JsonElement trackPluginInfoElement);
-
-            LavaTrack track = JsonSerializer.Deserialize<LavaTrack>(trackElement);
-            track.Hash = trackHashElement.ToString();
-            track.PluginInfo = trackPluginInfoElement.ToString();
-
-            return track;
-        }
+        using var trackDocument = JsonDocument.ParseValue(ref reader);
+        
+        trackDocument.RootElement.TryGetProperty("encoded", out var trackHashElement);
+        trackDocument.RootElement.TryGetProperty("info", out var trackElement);
+        trackDocument.RootElement.TryGetProperty("pluginInfo", out var trackPluginInfoElement);
+        
+        var track = trackElement.Deserialize<LavaTrack>();
+        track.Hash = trackHashElement.ToString();
+        track.PluginInfo = trackPluginInfoElement.ToString();
+        
+        return track;
     }
-
+    
     public override void Write(Utf8JsonWriter writer, LavaTrack value, JsonSerializerOptions options) {
         throw new NotImplementedException();
     }
--- src/Converters/LavaTrackListConverter.cs
+++ src/Converters/LavaTrackListConverter.cs
@@ -1,29 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Victoria.Converters;
 
-internal sealed class LavaTrackListConverter : JsonConverter<IReadOnlyCollection<LavaTrack>>
-{
-    public override IReadOnlyCollection<LavaTrack> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        using (JsonDocument trackDocument = JsonDocument.ParseValue(ref reader))
-        {
-            List<LavaTrack> trackList = new List<LavaTrack>();
-            foreach (JsonElement element in trackDocument.RootElement.EnumerateArray())
-            {
-                trackList.Add(JsonSerializer.Deserialize<LavaTrack>(element, Extensions.Options));
-            }
-
-            return new ReadOnlyCollection<LavaTrack>(trackList);
-        }
+internal sealed class LavaTrackListConverter : JsonConverter<IReadOnlyCollection<LavaTrack>> {
+    public override IReadOnlyCollection<LavaTrack> Read(ref Utf8JsonReader reader,
+                                                        Type typeToConvert,
+                                                        JsonSerializerOptions options) {
+        using var trackDocument = JsonDocument.ParseValue(ref reader);
+        return trackDocument.RootElement
+            .EnumerateArray()
+            .Select(element => element.Deserialize<LavaTrack>(Extensions.Options))
+            .ToList();
     }
-
-    public override void Write(Utf8JsonWriter writer, IReadOnlyCollection<LavaTrack> value, JsonSerializerOptions options)
-    {
+    
+    public override void Write(Utf8JsonWriter writer, IReadOnlyCollection<LavaTrack> value,
+                               JsonSerializerOptions options) {
         throw new NotImplementedException();
     }
 }
